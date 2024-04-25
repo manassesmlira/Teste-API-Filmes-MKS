6b293072755c739888d084d19bb50c76d2a7048b
Manasses_M_Lira <127964940+manassesmlira@users.noreply.github.com>
2024-04-25 12:42:18 -0300

    terminando configuracao do redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,39 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { FilmModule } from './film/film.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import config from './config';
 import { redisStore } from 'cache-manager-redis-yet';
 
 
 
 @Module({
   imports: [
-    CacheModule.register(
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync(
       {
         isGlobal: true,
-        ttl: 30 * 1000,
-        store: redisStore,
+        useFactory: async (config) => {
+          const store = await redisStore({
+            ttl: 30 * 1000,
+            socket: {
+              host: config.get('redis.host'),
+              port: config.get('redis.port'),
+            }
+          });
+          return {store};
+        },
+        inject: [ConfigService],
       }
     ),
 
-    ConfigModule.forRoot(),
+   
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
